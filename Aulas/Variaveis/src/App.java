0a0f09f527ca44841f4e146059ddf51565acc764
JPMends <139639644+jpmendes2000@users.noreply.github.com>
2025-03-31 18:10:01 -0300

    Delete Aulas/Variaveis directory

--- Aulas/Variaveis/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        double nota = 6.5;
-        int notaInteira = (int) nota; // vira "6" kkkkkk tirou menos que devia, bobão.
-
-        System.out.println("sua nota é " + notaInteira);
-    }
-}
