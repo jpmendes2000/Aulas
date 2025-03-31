5cdd7946eacf1c6c39567ff0596ed24e64069447
JPMends <139639644+jpmendes2000@users.noreply.github.com>
2025-03-31 18:06:24 -0300

    Update App.java

--- Aulas/Variaveis/src/App.java
+++ Aulas/Variaveis/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double nota = 6.5;
+        int notaInteira = (int) nota; // vira "6" kkkkkk tirou menos que devia, bobão.
+
+        System.out.println("sua nota é " + notaInteira);
     }
 }
