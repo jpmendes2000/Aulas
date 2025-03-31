df7455c0500abdac861b6e5a2993527910acc5da
JPMends <139639644+jpmendes2000@users.noreply.github.com>
2025-03-31 17:51:57 -0300

    Update App.java
    sintaxes basicas

--- Aulas/Sintaxes-basicas/src/App.java
+++ Aulas/Sintaxes-basicas/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String nome = "João Pedro Mendes";
+        int idade = 18;
+        float saldo = 9.15f;
+        double ate_Onde_Ele_Sabe_Que_Vai_O_PI = 3.14159265359;
+
+        System.out.println("ele é o " + nome);
+        System.out.println("O " + nome + " tem " + idade + " anos.");
+        System.out.println("ele tem guardado R$" + saldo );
+        System.out.println("o máximo que ele sabe do pi é " + ate_Onde_Ele_Sabe_Que_Vai_O_PI);
     }
 }
