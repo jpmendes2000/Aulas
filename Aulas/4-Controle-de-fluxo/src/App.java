d14fc80c7d20dce859f9e59fa37376b45cda6504
JPMends <139639644+jpmendes2000@users.noreply.github.com>
2025-03-31 18:54:54 -0300

    Update App.java

--- Aulas/4-Controle-de-fluxo/src/App.java
+++ Aulas/4-Controle-de-fluxo/src/App.java
@@ -1,5 +1,34 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // if, else if e else 
+        int idade = 17;
+
+        if (idade >= 18) {
+            System.out.println("pode ir pra baladinha");
+        } else if (idade <= 17) {
+            System.out.println("MENOR DE IDADE, SAI DAQUI SEU LOUCO");
+        } else {
+            System.out.println("oxiii quantos anos você me daria");
+        }
+
+
+        System.out.println("");
+        // for
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("O número é " + i);
+          }
+
+
+        System.out.println("");
+        // while 
+
+
+
+        int contador = 0;
+        while (contador <= 10) {
+            System.out.println("Contando... " + contador);
+        contador++;
+}
     }
 }
