bb0120a704729166790c83743fa59ff8c9b48fe0
JPMends <139639644+jpmendes2000@users.noreply.github.com>
2025-03-31 17:57:02 -0300

    Update App.java

--- Aulas/Sintaxes-basicas/src/App.java
+++ Aulas/Sintaxes-basicas/src/App.java
@@ -5,10 +5,17 @@ public class App {
         int idade = 18;
         float saldo = 9.15f;
         double ate_Onde_Ele_Sabe_Que_Vai_O_PI = 3.14159265359;
+        boolean homem = true;
 
         System.out.println("ele é o " + nome);
         System.out.println("O " + nome + " tem " + idade + " anos.");
         System.out.println("ele tem guardado R$" + saldo );
         System.out.println("o máximo que ele sabe do pi é " + ate_Onde_Ele_Sabe_Que_Vai_O_PI);
+        
+        if (homem == true) {
+            System.out.println("ele é um macho");
+        } else {
+            System.out.println("aaaa uma mulher, credo");
+        }
     }
 }
