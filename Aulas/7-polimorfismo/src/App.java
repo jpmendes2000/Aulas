8ecc2324376767ee51fd9d694ea7434771fe02b9
JPMends <139639644+jpmendes2000@users.noreply.github.com>
2025-04-01 00:00:17 -0300

    Update App.java

--- Aulas/7-polimorfismo/src/App.java
+++ Aulas/7-polimorfismo/src/App.java
@@ -1,5 +1,9 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+      Animal animal1 = new Cachorro();
+      Animal animal2 = new Gato();
+  
+      animal1.emitirSom(); 
+      animal2.emitirSom(); 
     }
-}
+  }
